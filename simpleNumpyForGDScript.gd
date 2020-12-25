00e9a9789be0b77364fa5b87fabc3694e1a557b3
Reda Meyer <reda.meyer@hydrometrie.ch>
2020-12-25 18:14:13 +0100

    1.0.0

---
+++ simpleNumpyForGDScript.gd
@@ -0,0 +1,52 @@
+extends Node
+
+
+
+var random: RandomNumberGenerator = RandomNumberGenerator.new()
+
+
+func zeros(matLengthX, matLengthY, matLengthZ=0):
+	var matrix = []
+	for x in range(matLengthX):
+		matrix.append([])
+		for y in range(matLengthY):
+			matrix[x].append(0)
+	return matrix
+		
+func matmul(mat1, mat2):
+	#Init with zeros
+	var result = zeros(len(mat1), len(mat2[0]))
+	# iterate through rows of r1
+	for x in range(len(mat1)):
+		# iterate through columns of c2
+		for y in range(len(mat2[0])):
+			# iterate through rows of r2
+			for z in range(len(mat2)):
+				result[x][y] += mat1[x][z] * mat2[z][y]
+				
+	return result
+func randn(mat):
+	#Init with zeros
+	var result = zeros(len(mat), len(mat))
+	for x in range(len(mat)):
+		for y in range(len(mat)):
+			result[x][y] = random.randfn(1,1)
+	return result
+	
+func reshape(mat,newSizeX,newSizeY):
+#	var count = mat.size() +  mat[0].size()
+	var newMat = zeros(newSizeX,newSizeY)
+#	if newSizeX + newSizeY !=  mat.size() +  mat[0].size():
+#		return -1 
+#	else:
+	
+	var x = 0
+	var y = 0
+	for reshapeX in range(newSizeX):
+		for reshapeY in range(newSizeY):
+			newMat[reshapeX][reshapeY] = mat[x][y]
+			y += 1
+			if y >= len(mat[0]):
+				y = 0
+				x += 1
+	return newMat
