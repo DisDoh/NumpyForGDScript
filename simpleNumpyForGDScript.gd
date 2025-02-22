dabd4e8b3c87cdbd474b22337ac18880bc292cf0
DisD <disd.mrb@gmail.com>
2025-02-22 12:00:48 +0100

    Added 3D operations for all Functions

--- simpleNumpyForGDScript.gd
+++ simpleNumpyForGDScript.gd
@@ -1,4 +1,5 @@
 extends Node
+
 var random: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func zeros(matLengthX, matLengthY, matLengthZ=0):
@@ -18,97 +19,175 @@ func zeros(matLengthX, matLengthY, matLengthZ=0):
 	return matrix
 
 func matmul(mat1, mat2):
-	if len(mat1[0]) != len(mat2):
-		push_error("Erreur: Dimensions incompatibles pour la multiplication de matrices")
-		return []
-	var result = zeros(len(mat1), len(mat2[0]))
-	for x in range(len(mat1)):
-		for y in range(len(mat2[0])):
-			for z in range(len(mat2)):
-				result[x][y] += mat1[x][z] * mat2[z][y]
-	return result
+	if mat1.size() > 0 and mat1[0].size() > 0 and mat1[0][0] is Array:
+		if mat1.size() != mat2.size():
+			push_error("Erreur: Nombre de batches incompatibles pour la multiplication de matrices 3D")
+			return []
+		var result = []
+		for i in range(mat1.size()):
+			result.append(matmul(mat1[i], mat2[i]))
+		return result
+	else:
+		if len(mat1[0]) != len(mat2):
+			push_error("Erreur: Dimensions incompatibles pour la multiplication de matrices")
+			return []
+		var result = zeros(len(mat1), len(mat2[0]))
+		for x in range(len(mat1)):
+			for y in range(len(mat2[0])):
+				for z in range(len(mat2)):
+					result[x][y] += mat1[x][z] * mat2[z][y]
+		return result
 
-func randn(sizeX, sizeY):
-	var result = zeros(sizeX, sizeY)
+func randn(sizeX, sizeY, sizeZ=0):
+	var result = zeros(sizeX, sizeY, sizeZ)
 	for x in range(sizeX):
 		for y in range(sizeY):
-			result[x][y] = random.randfn(0, 1)
+			if sizeZ == 0:
+				result[x][y] = random.randfn(0, 1)
+			else:
+				for z in range(sizeZ):
+					result[x][y][z] = random.randfn(0, 1)
 	return result
 
-func reshape(mat, newSizeX, newSizeY):
-	var total_elements = len(mat) * len(mat[0])
-	if total_elements != newSizeX * newSizeY:
-		push_error("Erreur: Impossible de redimensionner, tailles incompatibles")
-		return []
-	var newMat = zeros(newSizeX, newSizeY)
+func reshape(mat, newSizeX, newSizeY, newSizeZ=0):
 	var elements = []
-	for row in mat:
-		elements.append_array(row)
-	var index = 0
-	for reshapeX in range(newSizeX):
-		for reshapeY in range(newSizeY):
-			newMat[reshapeX][reshapeY] = elements[index]
-			index += 1
-	return newMat
+	if mat.size() == 0:
+		push_error("Erreur: matrice vide")
+		return []
+	if mat[0].size() > 0 and mat[0][0] is Array:
+		for x in mat:
+			for y in x:
+				for z in y:
+					elements.append(z)
+	else:
+		for row in mat:
+			elements.append_array(row)
+
+	if newSizeZ == 0:
+		if elements.size() != newSizeX * newSizeY:
+			push_error("Erreur: Impossible de redimensionner, tailles incompatibles")
+			return []
+		var newMat = zeros(newSizeX, newSizeY)
+		var index = 0
+		for x in range(newSizeX):
+			for y in range(newSizeY):
+				newMat[x][y] = elements[index]
+				index += 1
+		return newMat
+	else:
+		if elements.size() != newSizeX * newSizeY * newSizeZ:
+			push_error("Erreur: Impossible de redimensionner, tailles incompatibles")
+			return []
+		var newMat = zeros(newSizeX, newSizeY, newSizeZ)
+		var index = 0
+		for x in range(newSizeX):
+			for y in range(newSizeY):
+				for z in range(newSizeZ):
+					newMat[x][y][z] = elements[index]
+					index += 1
+		return newMat
 
 func transpose(mat):
-	var transposed = zeros(len(mat[0]), len(mat))
-	for x in range(len(mat)):
-		for y in range(len(mat[0])):
-			transposed[y][x] = mat[x][y]
-	return transposed
+	if mat.size() > 0 and mat[0].size() > 0 and mat[0][0] is Array:
+		var transposed = zeros(len(mat[0]), len(mat), len(mat[0][0]))
+		for x in range(len(mat)):
+			for y in range(len(mat[0])):
+				for z in range(len(mat[0][0])):
+					transposed[y][x][z] = mat[x][y][z]
+		return transposed
+	else:
+		var transposed = zeros(len(mat[0]), len(mat))
+		for x in range(len(mat)):
+			for y in range(len(mat[0])):
+				transposed[y][x] = mat[x][y]
+		return transposed
 
-func identity(size):
-	var mat = zeros(size, size)
-	for i in range(size):
-		mat[i][i] = 1
-	return mat
+func identity(size, batch=0):
+	if batch == 0:
+		var mat = zeros(size, size)
+		for i in range(size):
+			mat[i][i] = 1
+		return mat
+	else:
+		var mat = zeros(batch, size, size)
+		for b in range(batch):
+			for i in range(size):
+				mat[b][i][i] = 1
+		return mat
 
 func print_matrix(mat):
-	for row in mat:
-		print(row)
+	if mat.size() > 0 and mat[0].size() > 0 and mat[0][0] is Array:
+		for i in range(len(mat)):
+			print("Slice ", i, ":")
+			for row in mat[i]:
+				print(row)
+	else:
+		for row in mat:
+			print(row)
 
-# Function to compare two matrices using a tolerance (to account for floating point differences)
 func matrices_equal(mat1, mat2, tol=0.0001):
-	if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+	if mat1.size() != mat2.size():
 		return false
-	for x in range(len(mat1)):
-		for y in range(len(mat1[0])):
-			if abs(mat1[x][y] - mat2[x][y]) > tol:
-				return false
-	return true
+	if mat1[0].size() > 0 and mat1[0][0] is Array:
+		if len(mat1[0]) != len(mat2[0]) or len(mat1[0][0]) != len(mat2[0][0]):
+			return false
+		for x in range(len(mat1)):
+			for y in range(len(mat1[0])):
+				for z in range(len(mat1[0][0])):
+					if abs(mat1[x][y][z] - mat2[x][y][z]) > tol:
+						return false
+		return true
+	else:
+		if len(mat1[0]) != len(mat2[0]):
+			return false
+		for x in range(len(mat1)):
+			for y in range(len(mat1[0])):
+				if abs(mat1[x][y] - mat2[x][y]) > tol:
+					return false
+		return true
 
 func test_zeros():
-	var start_time = Time.get_ticks_msec()
 	var mat = zeros(2, 3)
-	var end_time = Time.get_ticks_msec()
-	print("zeros() took: ", end_time - start_time, " ms")
-	assert(matrices_equal(mat, [[0, 0, 0], [0, 0, 0]]), "Error in zeros()")
+	assert(matrices_equal(mat, [[0, 0, 0], [0, 0, 0]]), "Error in zeros() 2D")
+
+func test_zeros_3d():
+	var mat = zeros(2, 3, 4)
+	assert(mat.size() == 2 and mat[0].size() == 3 and mat[0][0].size() == 4, "Error in zeros() 3D")
 
 func test_matmul():
 	var mat1 = [[1, 2], [3, 4]]
 	var mat2 = [[2, 0], [1, 2]]
 	var expected = [[4, 4], [10, 8]]
-	var start_time = Time.get_ticks_msec()
 	var result = matmul(mat1, mat2)
-	var end_time = Time.get_ticks_msec()
-	print("matmul() took: ", end_time - start_time, " ms")
-	assert(matrices_equal(result, expected), "Error in matmul()")
+	assert(matrices_equal(result, expected), "Error in matmul() 2D")
+
+func test_matmul_3d():
+	var mat1 = [[[1, 2], [3, 4]], [[2, 0], [1, 2]]]
+	var mat2 = [[[2, 0], [1, 2]], [[1, 1], [0, 1]]]
+	var expected = [[[4, 4], [10, 8]], [[2, 2], [1, 3]]]
+	var result = matmul(mat1, mat2)
+	assert(matrices_equal(result, expected), "Error in matmul() 3D")
 
 func test_transpose():
 	var mat = [[1, 2, 3], [4, 5, 6]]
 	var expected = [[1, 4], [2, 5], [3, 6]]
-	var start_time = Time.get_ticks_msec()
 	var result = transpose(mat)
-	var end_time = Time.get_ticks_msec()
-	print("transpose() took: ", end_time - start_time, " ms")
-	assert(matrices_equal(result, expected), "Error in transpose()")
+	assert(matrices_equal(result, expected), "Error in transpose() 2D")
+
+func test_transpose_3d():
+	var mat = [[[1, 2], [3, 4], [5, 6]], [[7, 8], [9, 10], [11, 12]]]
+	var expected = [[[1, 2], [7, 8]], [[3, 4], [9, 10]], [[5, 6], [11, 12]]]
+	var result = transpose(mat)
+	assert(matrices_equal(result, expected), "Error in transpose() 3D")
 
 func run_tests():
-	print("Launching all tests with time tracking...")
+	print("Running all tests...")
 	test_zeros()
+	test_zeros_3d()
 	test_matmul()
+	test_matmul_3d()
 	test_transpose()
+	test_transpose_3d()
 	print("All tests passed! ✅")
 
 func _ready():
